0e2405b47eb2083ed752509daccf87aee0920bf3
bbkincso <bbkincso@gmail.com>
2022-04-20 00:31:32 +0100

    Adding Application Java source

---
+++ src/main/java/ait/course/example/Application.java
@@ -0,0 +1,31 @@
+package ait.course.example;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+
+public class Application {
+
+// public int countWords(String words){
+//   String[] seperateWords = StringUtils.split(words,' ');
+//   return (seperateWords == null)? 0:seperateWords.length;
+// }
+
+// public void greet(){
+// 	List<String> greetings = new ArrayList<>();
+// 	greetings.add("Hello");
+//      for (String greeting : greetings){
+// 		System.out.println("Greeting: "+greeting);
+// 	}
+// }
+
+public Application(){
+System.out.println("Inside Application");
+}
+
+public static void main(String[] args){
+	System.out.println("Starting Application");
+	Application app = new Application();
+    //  app.greet();
+}
+
+}
