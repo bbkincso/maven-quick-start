16f04ab029f58955855d1d2b3954d34de8687316
bbkincso <bbkincso@gmail.com>
2022-04-20 00:52:39 +0100

    added countwords method to Application.java

--- src/main/java/ait/course/example/Application.java
+++ src/main/java/ait/course/example/Application.java
@@ -5,10 +5,10 @@ import org.apache.commons.lang3.StringUtils;
 
 public class Application {
 
-// public int countWords(String words){
-//   String[] seperateWords = StringUtils.split(words,' ');
-//   return (seperateWords == null)? 0:seperateWords.length;
-// }
+ public int countWords(String words){
+   String[] seperateWords = StringUtils.split(words,' ');
+   return (seperateWords == null)? 0:seperateWords.length;
+ }
 
  public void greet(){
  	List<String> greetings = new ArrayList<>();
@@ -26,6 +26,8 @@ public static void main(String[] args){
 	System.out.println("Starting Application");
 	Application app = new Application();
       app.greet();
+	  int count = app.countWords("I have four words");
+	  System.out.println("Word count: " +count);
 }
 
 }
